ec1836a2d6e7a20029d5dd7e143d94bde5d204f3
dkelly617 <dkelly617@gmail.com>
2019-06-02 17:38:25 -0400

    second
    second

--- ConnectionChk.py
+++ ConnectionChk.py
@@ -1,4 +1,8 @@
-import site
+import pipenv.vendor.requests as requests
 
-site.getsitepackages()
+response = requests.get('https://httpbin.org/ip')
+
+print('Your IP is {0}'.format(response.json()['origin']))
+
+
 
